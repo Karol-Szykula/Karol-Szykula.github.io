bc77c80da5c897b7918b08fb2881e61bbb3bd405
Karol-Szykula <karol.szykula@wp.pl>
2018-10-22 21:33:39 +0200

    Move navbar outside the container div

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const menuToggle = document.querySelector('.container__nav-top__toggle__link');
-const menuList = document.querySelector('.container__nav-top__flex-wrapper');
-const menuListItems = document.querySelectorAll('.container__nav-top__flex-wrapper__menu-item');
+const menuToggle = document.querySelector('.nav-top__toggle__link');
+const menuList = document.querySelector('.nav-top__flex-wrapper');
+const menuListItems = document.querySelectorAll('.nav-top__flex-wrapper__menu-item');
 
 const menuListHeight = menuListItems[0].clientHeight * menuListItems.length;
 //console.log(menuListHeight);
-menuList.classList.add('container__nav-top__flex-wrapper--collapsed');
+menuList.classList.add('nav-top__flex-wrapper--collapsed');
 
 menuToggle.addEventListener(
     'click',
     (event) => {
         event.preventDefault();
 
-        if(menuList.classList.contains('container__nav-top__flex-wrapper--collapsed')){
-            menuList.classList.remove('container__nav-top__flex-wrapper--collapsed');
+        if(menuList.classList.contains('nav-top__flex-wrapper--collapsed')){
+            menuList.classList.remove('nav-top__flex-wrapper--collapsed');
             menuList.style.maxHeight = menuListHeight + 'px';
         }else{
-            menuList.classList.add('container__nav-top__flex-wrapper--collapsed');
+            menuList.classList.add('nav-top__flex-wrapper--collapsed');
         }
 
 
